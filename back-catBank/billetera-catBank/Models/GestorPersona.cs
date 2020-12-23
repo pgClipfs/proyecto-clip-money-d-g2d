3df6842dd554d9bd960715733863ee31b1688db4
Bluemary04 <46400899+Bluemary04@users.noreply.github.com>
2020-12-23 13:15:21 -0300

    registro de persona

--- back-catBank/billetera-catBank/Models/GestorPersona.cs
+++ back-catBank/billetera-catBank/Models/GestorPersona.cs
@@ -13,8 +13,11 @@ namespace AngularMVCProject.Models
        
         public int AgregarPersona(Persona nueva)
         {
-            string StrConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+            //string StrConn = ConfigurationManager.ConnectionStrings["dbHomeBank"].ToString();
+            string StrConn = "Data Source=AR-IT02462\\SQLEXPRESS01;Initial Catalog=dbHomeBank;Integrated Security=True";
+
             int id=0;
+            int dni = 0;
 
             using (SqlConnection conn = new SqlConnection(StrConn))
             {
@@ -23,10 +26,18 @@ namespace AngularMVCProject.Models
                     conn.Open();
 
                     SqlCommand comm = conn.CreateCommand();
-                    comm.CommandText = "pa_insertar_usuario";
+                    comm.CommandText = "pa_insertar_cliente";
                     comm.CommandType = System.Data.CommandType.StoredProcedure;
                     comm.Parameters.Add(new SqlParameter("@Nombre", nueva.Nombre));
                     comm.Parameters.Add(new SqlParameter("@Apellido", nueva.Apellido));
+                    comm.Parameters.Add(new SqlParameter("@Dni", nueva.Dni));
+                    comm.Parameters.Add(new SqlParameter("@Pais", nueva.Pais));
+                    comm.Parameters.Add(new SqlParameter("@Localidad", nueva.Localidad));
+                    comm.Parameters.Add(new SqlParameter("@Mail", nueva.Mail));
+                    comm.Parameters.Add(new SqlParameter("@Telefono", nueva.Telefono));
+                    comm.Parameters.Add(new SqlParameter("@Pass", nueva.Pass));
+                    comm.Parameters.Add(new SqlParameter("@Usuario", nueva.Usuario));
+
 
                     id = Convert.ToInt32(comm.ExecuteScalar());
                 }
@@ -60,9 +71,16 @@ namespace AngularMVCProject.Models
                         int id = dr.GetInt32(0);
                         string nombre = dr.GetString(1).Trim();
                         string apellido = dr.GetString(2).Trim();
+                        string dni = dr.GetString(9).Trim();
+                        string pais = dr.GetInt32(3).ToString();
+                        string localidad = dr.GetString(4).Trim();
+                        string mail = dr.GetString(5).Trim();
+                        string telefono = dr.GetString(6).Trim();
+                        string pass = dr.GetString(7).Trim();
+                        string usuario = dr.GetString(8).Trim();
 
-                        Persona p = new Persona(id, nombre, apellido);
-                        lista.Add(p);
+                    Persona p = new Persona(id, nombre, apellido, pais, localidad, mail, telefono, pass, usuario, dni);
+                    lista.Add(p);
                     }
 
                     dr.Close();
@@ -106,8 +124,15 @@ namespace AngularMVCProject.Models
                 {
                     string nombre = dr.GetString(1);
                     string apellido = dr.GetString(2);
+                    string dni = dr.GetString(9).Trim();
+                    string pais = dr.GetString(3).Trim();
+                    string localidad = dr.GetString(4).Trim();
+                    string mail = dr.GetString(5).Trim();
+                    string telefono = dr.GetString(6).Trim();
+                    string pass = dr.GetString(7).Trim();
+                    string usuario = dr.GetString(8).Trim();
 
-                    p = new Persona(id, nombre, apellido);
+                    p = new Persona(id, nombre, apellido, pais, localidad, mail, telefono, pass, usuario, dni);
                 }
 
                 dr.Close();
