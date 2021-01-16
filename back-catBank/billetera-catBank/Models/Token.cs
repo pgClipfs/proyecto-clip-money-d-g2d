110c7e585cc5628d587807034955565d3ffbdf32
Bluemary04 <46400899+Bluemary04@users.noreply.github.com>
2021-01-16 13:33:20 -0300

    --

---
+++ back-catBank/billetera-catBank/Models/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularMVCProject.Models
+{
+    public class Token
+    {
+        private string token;
+
+        public string Jwt { get => token; set => token = value; }
+    }
+}
